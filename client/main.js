69f59ede5f91fc795f58c497637bb5fdaed5cf3b
tamino-martinius <dev@zaku.eu>
2016-07-07 10:46:30 +0200

    Updated scrum master offset

--- client/main.js
+++ client/main.js
@@ -5,6 +5,6 @@ Template.app.helpers({
     return scrumMasters.join(', ');
   },
   scrumMaster() {
-    return scrumMasters[(Math.floor((Date.now()/1e3/60/60/24+3)/7)+2) % scrumMasters.length];
+    return scrumMasters[(Math.floor((Date.now()/1e3/60/60/24+3)/7)+3) % scrumMasters.length];
   },
 });
