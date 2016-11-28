abf84489336b9a79c4a96379b71bfc0383e7c253
tamino-martinius <dev@zaku.eu>
2016-11-28 10:57:56 +0100

    Updated scrum masters

--- client/main.js
+++ client/main.js
@@ -1,10 +1,10 @@
-const scrumMasters = ["Jan","Shreyas","Tamino", "Noelia"]
+const scrumMasters = ["Jan", "Shreyas","Tamino", "Slava", "Noelia"]
 
 Template.app.helpers({
   scrumMasters() {
     return scrumMasters.join(', ');
   },
   scrumMaster() {
-    return scrumMasters[(Math.floor((Date.now()/1e3/60/60/24+3)/7)+3) % scrumMasters.length];
+    return scrumMasters[(Math.floor((Date.now()/1e3/60/60/24+3)/7)+4) % scrumMasters.length];
   },
 });
